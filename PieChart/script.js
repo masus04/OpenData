d314377632ebdde88895f6204af43474daef282b
masus04 <thomas_st@hotmail.com>
2015-04-30 10:18:53 +0200

    some more refactoring

--- PieChart/script.js
+++ PieChart/script.js
@@ -1,4 +1,4 @@
-var width = 1200,
+var width = 1000,
     height =800,
     color = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf", "#636363"];
 
